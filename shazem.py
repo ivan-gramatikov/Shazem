577c49edd4be549cc3b6c323c47ca694b4ac67e5
Ivan Gramatikov <39277343+ivan-gramatikov@users.noreply.github.com>
2018-08-24 13:42:57 +0300

    Add files via upload

---
+++ shazem.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+songlist = []
+try:
+   conn = sqlite3.connect('library.db')
+except IOError:
+   print("Could not open the database")
+
+cursor = conn.execute("SELECT artist_name, title from track")
+for row in cursor:
+   singlesong = row[0], row[1]
+   songlist.append(singlesong)
+
+# Take the songlist and look at the lines below for a hint ;) You know what to do...
+
+# for element in songlist:
+#    youtube - dl "ytsearch1: ......"
+
+conn.close()
